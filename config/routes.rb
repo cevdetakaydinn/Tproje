f218b7d498351d9d7b126bacbada3a628e81e240
cevdet akaydin <cevdet.akaydin@bil.omu.edu.tr>
2017-06-22 13:59:32 +0300

    admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :subcategories
   resources :categories
   root to: "home#index"
+  resources :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
